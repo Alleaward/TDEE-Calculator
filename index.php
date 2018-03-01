f356195ddd4158b3d7704fcb9cfd3ac27fb68b18
Allea Ward <Allea_ward@hotmail.com>
2018-03-01 17:08:11 +1000

    Added some JS scripts for interactivity and data submission

--- index.php
+++ index.php
@@ -13,85 +13,93 @@
 </head>
 
 <body>
-
-    <div class="container">
-      <div class="col s2"></div>
-        <form id="newInput" class="col s8 center-align">
-            <p>TDEE Calculator</p>
-            <div class="row">
-                <div class="input-field col s4">
-                    <input id="weight" type="text" class="validate">
-                    <label for="weight">Weight(kg):</label>
+    <section id="main">
+        <div class="container">
+          <div class="col s2"></div>
+            <form id="newInput" class="col s8 center-align">
+                <p>TDEE Calculator</p>
+                <div class="row">
+                    <div class="input-field col s4">
+                        <input id="weight" type="number" class="validate" name="Weight" minlength="2" maxlength="3" required>
+                        <label for="weight">Weight(kg):</label>
+                    </div>
+                    <div class="input-field col s4">
+                        <input id="height" type="text" class="validate" name="Height" minlength="3" maxlength="3" required>
+                        <label for="height">Height(cm):</label>
+                    </div>
+                    <div class="input-field col s4">
+                        <input id="age" type="text" class="validate" name="Age" minlength="1" maxlength="2" required>
+                        <label for="age">Age:</label>
+                    </div>
+                    <div class="input-field col s12" name="Activity" id="Activity">
+                        <select>
+                            <option value="1.2" selected>Sedentary: Little or no Exercise, Desk job</option>
+                            <option value="1.375">Lightly active: Light exercise, Sports 1 &ndash; 3 days per week</option>
+                            <option value="1.55">Moderately active: Moderate Exercise, Sports 3 &ndash; 5 days per week</option>
+                            <option value="1.725">Very active: Heavy Exercise, Sports 6 &ndash; 7 days per week</option>
+                            <option value="1.9">Extremely active: Very heavy exercise, Physical Job or Training 2x per day</option>
+                        </select>
+                        <label>Activity Level:</label>
+                    </div>
+                    <button id="newUserSubmit" class="btn waves-effect waves-light" type="submit" name="action">Calculate
+                        <i class="material-icons right">send</i>
+                    </button>
                 </div>
-                <div class="input-field col s4">
-                    <input id="height" type="text" class="validate">
-                    <label for="height">Height(cm):</label>
-                </div>
-                <div class="input-field col s4">
-                    <input id="age" type="text" class="validate">
-                    <label for="age">Age:</label>
-                </div>
-                <div class="input-field col s12" name="Activity" id="Activity">
-                    <select>
-                        <option value="" disabled selected>How often do you exercise?</option>
-                        <option value="1.2">Sedentary: Little or no Exercise, Desk job</option>
-                        <option value="1.375">Lightly active: Light exercise, Sports 1 &ndash; 3 days per week</option>
-                        <option value="1.55">Moderately active: Moderate Exercise, Sports 3 &ndash; 5 days per week</option>
-                        <option value="1.725">Very active: Heavy Exercise, Sports 6 &ndash; 7 days per week</option>
-                        <option value="1.9">Extremely active: Very heavy exercise, Physical Job or Training 2x per day</option>
-                    </select>
-                    <label>Activity Level:</label>
-                </div>
-            </div>
-            <button class="btn waves-effect waves-light" type="submit" name="action">Calculate
-                <i class="material-icons right">send</i>
-            </button>
-        </form>
-      <div class="col s2"></div>
-    </div>
-
-    <div class="container">
-      <div class="col s2"></div>
-        <form id="loginInfo" class="col s8 center-align">
-            <p>Already been here? Login!</p>
-            <div class="row">
-                <div class="input-field col s12 l6 center-align">
-                    <input id="Username" type="text" class="validate">
-                    <label for="Username">Username:</label>
-                </div>
-                <div class="input-field col s12 l6 center-align">
-                    <input id="Password" type="password" class="validate">
-                    <label for="Password">Password:</label>
-                </div>
-            </div>
-            <button class="btn waves-effect waves-light" type="submit" name="action">Login
-                <i class="material-icons right">send</i>
-            </button>
-        
-        </form>
-      <div class="col s2"></div>
-    </div>
-
-    <div class="container">
-      <div class="col s2"></div>
-        <form id="userInfo" class="col s8 center-align">
-            <p>Your information</p>
-            <p class="weight"></p>
-            <p class="height"></p>
-            <p class="age"></p>
-            <p class="activity"></p>
-            <p class="tdee"></p>
-            <form id="signUpForm" class="" action="" method="">
-                <input class="col-6" type="text" name="Username" id="Username" placeholder="Username:" minlength="5" required>
-                <input class="col-6" type="password" name="Password" id="Password" placeholder="Password:" minlength="5" required>
-                <input type="submit" name="signUpButton" class="" onclick="" value="Save Data">
-                <input type="submit" id="returnButton" name="submit" class="" onclick="" value="Return">
             </form>
-        </form>
-      <div class="col s2"></div>
-    </div>
+          <div class="col s2"></div>
+        </div>
+    
+        <div class="container">
+          <div class="col s2"></div>
+            <form id="loginInfo" class="col s8 center-align">
+                <p>Already been here? Login!</p>
+                <div class="row">
+                    <div class="input-field col s12 l6 center-align">
+                        <input id="LoginUsername" type="text" class="validate" name="LoginUsername" minlength="5" required>
+                        <label for="LoginUsername">Username:</label>
+                    </div>
+                    <div class="input-field col s12 l6 center-align">
+                        <input id="LoginPassword" type="password" class="validate" name="LoginPassword" minlength="5" autocomplete="on" required>
+                        <label for="LoginPassword">Password:</label>
+                    </div>
+                    <button id="loginSubmit" class="btn waves-effect waves-light" type="submit" name="action">Login
+                        <i class="material-icons right">send</i>
+                    </button>
+                </div>
+            </form>
+          <div class="col s2"></div>
+        </div>
+    
+        <div class="container">
+          <div class="col s2"></div>
+            <form id="userInfo" class="col s8 center-align hideDiv">
+                <p>Your information</p>
+                <p class="weight"></p>
+                <p class="height"></p>
+                <p class="age"></p>
+                <p class="activity"></p>
+                <p class="tdee"></p>
+                <form id="signUpForm" class="" action="" method="">
+                    <p>Enter a username and password to save your data. (Demo server, not secure!!!)</p>
+                    <input class="col s6" type="text" name="SignInUsername" id="Username" placeholder="Username:" minlength="5" required>
+                    <input class="col s6" type="password" name="SignInPassword" id="Password" placeholder="Password:" minlength="5" autocomplete="on" required>
+                    <button id="signUpButton" class="btn waves-effect waves-light" type="submit" name="signUpButton">Save Data
+                        <i class="material-icons right">send</i>
+                    </button>
+                    <button id="returnButton" class="btn waves-effect waves-light" type="submit" name="submit">Home
+                        <i class="material-icons right">send</i>
+                    </button>
+                </form>
+            </form>
+          <div class="col s2"></div>
+        </div>
+    </section>
 
-        
+    <footer class="container-fluid col s12 center-align">
+        <a href="http://alleaward.com/">&copy; Developed by Allea Ward</a>
+    </footer>
+
+
     <!--JavaScript at end of body for optimized loading-->
     <script type="text/javascript" src="js/materialize.min.js"></script>
     <script type="text/javascript" src="js/main.js"></script>
--- js/main.js
+++ js/main.js
@@ -1,1 +1,67 @@
 M.AutoInit();
+
+var newUserSubmit = document.querySelector('#newUserSubmit');
+var newUserDiv = document.querySelector('#newInput');
+
+var loginSubmit = document.querySelector('#loginSubmit');
+var loginDiv = document.querySelector('#loginInfo');
+
+var userInfoSave = document.querySelector('#signUpButton');
+var userInfo = document.querySelector('#userInfo');
+
+var userReturn = document.querySelector('#returnButton');
+// var ... = document.querySelector('#userInfo');
+
+
+newUserSubmit.addEventListener('submit', (e) => {
+    e.preventDefault();
+    e.stopPropagation();
+    console.log('newUserSubmit');
+    
+    //Get the values from the form.	
+    var Weight = +$('#Weight').val();
+    var Height = +$('#Height').val();
+    var Age = +$('#Age').val();
+    var Activity = +$('#Activity').val();
+    //Calculate
+    var bmr = (66 + (13.7 * Weight + (5 * Height) - (6.8 * Age)));
+    var tdee = Math.floor(bmr * Activity);
+    //Go to new view
+    $('#inputForm').slideUp().fadeOut();
+    $('#information').fadeIn().slideDown();
+    $('#loginForm').slideUp().fadeOut();
+    $('#signUpForm').fadeIn().slideDown();
+    //Insert the information.
+    $('.weight').replaceWith('<p class="weight">Weight(kg): ' + Weight + '</p>');
+    $('.height').replaceWith('<p class="height">Height(cm): ' + Height + '</p>');
+    $('.age').replaceWith('<p class="age">Age: ' + Age + '</p>');
+    $('.activity').replaceWith('<p class="activity">Activity: ' + Activity + '</p>');
+    $('.tdee').replaceWith('<p class="tdee">TDEE: ' + tdee + '</p>');
+
+    userInfo.classList.remove("hideDiv");
+    loginDiv.classList.add("hideDiv");
+    newUserDiv.classList.add("hideDiv");
+});
+
+loginSubmit.addEventListener('submit', (e) => {
+    e.preventDefault();
+    e.stopPropagation();
+    console.log('loginSubmit');
+    userInfo.classList.remove("hideDiv");
+    loginDiv.classList.add("hideDiv");
+});
+
+userInfoSave.addEventListener('submit', (e) => {
+    e.preventDefault();
+    e.stopPropagation();
+    console.log('userInfoSave');
+});
+
+userReturn.addEventListener('submit', (e) => {
+    e.preventDefault();
+    e.stopPropagation();
+    console.log('userReturn');
+    userInfo.classList.add("hideDiv");
+    loginDiv.classList.remove("hideDiv");
+    newUserDiv.classList.remove("hideDiv");
+});
